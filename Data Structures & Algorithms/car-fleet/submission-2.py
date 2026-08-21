219555c320f8aba1e017119dc5f57887d5b5b761
kc3222 <39278351+kc3222@users.noreply.github.com>
2026-08-21 12:49:43 -0400

    Add: car-fleet - submission-2

---
+++ Data Structures & Algorithms/car-fleet/submission-2.py
@@ -0,0 +1,19 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        # Sort the cars
+        # Compare two cars, if the earlier car has a slower speed, calculate if they will meet before target
+        # If they meet, calculate if they will meet for the next car
+        position, speed = zip(*sorted(zip(position, speed), reverse=True))
+        print(position)
+        print(speed)
+        fleets = [(position[0], speed[0])]
+        for i in range(1, len(position)):
+            currentFleetPos, currentFleetSpeed = fleets.pop()
+            currentFleetSteps = (target - currentFleetPos) / currentFleetSpeed
+            currentCarSteps = (target - position[i]) / speed[i]
+            if currentFleetSteps >= currentCarSteps:
+                fleets.append((currentFleetPos, currentFleetSpeed)) # Same fleet
+            else:
+                fleets.append((currentFleetPos, currentFleetSpeed))
+                fleets.append((position[i], speed[i]))
+        return len(fleets)
