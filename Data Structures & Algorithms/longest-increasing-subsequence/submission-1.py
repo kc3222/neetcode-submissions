78cc294365bf87b0167717da591537abdd5c9c38
kc3222 <39278351+kc3222@users.noreply.github.com>
2026-09-06 02:19:21 -0400

    Add: longest-increasing-subsequence - submission-1

---
+++ Data Structures & Algorithms/longest-increasing-subsequence/submission-1.py
@@ -0,0 +1,10 @@
+import bisect
+
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        res = [1 for i in range(len(nums))]
+        for i in range(1, len(nums)):
+            for j in range(i):
+                if nums[i] > nums[j]:
+                    res[i] = max(res[i], res[j] + 1)
+        return max(res)
